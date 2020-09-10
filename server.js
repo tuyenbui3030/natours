d7765b1f9c0ad25edcf9f49aa8d9247ab370b7fa
Anonymous <anonymous@ubuntu.ubuntu-domain>
2020-09-10 22:19:04 +0700

    Refactoring for MVC

--- server.js
+++ server.js
@@ -17,35 +17,6 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => console.log('DB connection successful!'));
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-const testTour = new Tour({
-  name: 'Bien ho - Pleiku',
-  rating: 4.8,
-  price: 450,
-});
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥:', err);
-  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
