abf6bda4c02791036c31600706a51ca3ecc954b6
Anonymous <anonymous@ubuntu.ubuntu-domain>
2020-09-10 14:31:25 +0700

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -17,7 +17,22 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => console.log('DB connection successful!'));
-
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Application starting on port ${port}!`);
