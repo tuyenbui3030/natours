048fc9624b1a36c488e31f2ff8b1e6ff844a3fbc
tuyenbui3030 <tuyenbui3030@gmail.com>
2020-09-24 21:38:41 +0700

    Global Error Handling Middleware

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./router/tourRoutes');
 const userRouter = require('./router/userRoutes');
 
 const app = express();
 
 //1 - MIDDLEWARE
-//console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -30,5 +32,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't fine ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 //4. START SERVER
 module.exports = app;
